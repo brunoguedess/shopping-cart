5e71f216ac9bb9932280ed4da6cc82fa15d7fcf4
Bruno Guedes <brunoguedess@gmail.com>
2018-05-14 20:48:47 -0300

    add restaurant list

--- frontend-meat-app/app/restaurants/restaurant/meat-restaurant.component.js
+++ frontend-meat-app/app/restaurants/restaurant/meat-restaurant.component.js
@@ -1,6 +1,9 @@
 // eslint-disable-next-line func-names
 (function () {
     angular.module('meat-app').component('meatRestaurant', {
+        bindings: {
+            restaurant: '='
+        },
         templateUrl: './restaurants/restaurant/meat-restaurant.component.html'
     });
 })();
